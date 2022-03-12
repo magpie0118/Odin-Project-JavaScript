eda0703ec110a211c99763bfab27c8956da00c5d
Maggie Fournier <mlfournier4@gmail.com>
2022-03-11 20:22:35 -0500

    Add files via upload

---
+++ The_Odin_Project/JavaScript_Practice/practice.js
@@ -0,0 +1,13 @@
+// MDN Learn - JS Functions
+
+const myText = 'I am a string'
+console.log(myText)
+const newString = myText.replace('string', 'sausage')
+console.log(newString);
+
+const myArray = ['I', 'love', 'chocolate', 'candy'];
+const madeAString = myArray.join(' ');
+console.log(madeAString);
+
+const myNumber = Math.random();
+console.log(myNumber)
